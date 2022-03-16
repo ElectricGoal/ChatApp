e623a9f6cb390d1b920ace8f2717236d941bf613
ElectricGoal <minhtu250802@gmail.com>
2022-03-16 22:16:55 +0700

    update

--- src/lib/screens/profile_screen.dart
+++ src/lib/screens/profile_screen.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chat_app/api/firebase_api.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
